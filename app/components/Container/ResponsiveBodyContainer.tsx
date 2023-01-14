7558c765f9946652e4158966f8f252d68ecb291e
Houla Sovansela <houlasovansela18@kit.edu.kh>
2023-01-14 22:44:44 +0700

    add nickname

--- app/components/Container/ResponsiveBodyContainer.tsx
+++ app/components/Container/ResponsiveBodyContainer.tsx
@@ -1,25 +1,52 @@
-import { FiBriefcase, FiCode, FiTerminal } from "react-icons/fi";
+import { FiBriefcase, FiCode, FiCpu } from "react-icons/fi";
 import ProfilePicture from "../Picture/ProfilePicture";
 import Rainbow from "../Stylish/Rainbow";
 
 export default function ResponsiveBodyContainer() {
-	const userName = "HOULA SOVANSELA";
-	const aboutMe= `I am a software developer graduated from Kirirom Institute of
-	Technology. I had experiences in web, mobile development AI,
-	machine learning and computer vision for almost 4 year.`
+	const userName = "houla sovansela";
+	const nickName = "godda";
+	const aboutMe = `I am a software developer graduated from Kirirom Institute of
+	Technology. I had experiences in website, mobile development, AI,
+	machine learning and computer vision.`;
+	const bio = [
+		// eslint-disable-next-line react/jsx-key
+		["software developer", <FiCode size={24}/>],
+		// eslint-disable-next-line react/jsx-key
+		["self employ", <FiBriefcase size={24}/>],
+		// eslint-disable-next-line react/jsx-key
+		["am geek!", <FiCpu size={24}/>],
+	];
 	return (
 		<div className="bg-transparent mx-auto h-full max-w-4xl px-8">
 			<Rainbow width={60} height={60} />
-			<div className="flex flex-col justify-between sm:flex-col md:flex-row lg:flex-row xl:flex-row 2xl:flex-row gap-16">
+			<div className="flex flex-col justify-between sm:flex-col md:flex-row lg:flex-row xl:flex-row 2xl:flex-row gap-20">
 				<div className="flex flex-1 justify-start flex-col space-y-8 text-justify">
-					<div>
-						<h1 className="font-bold sm:text-4xl text-3xl">{userName}</h1>
-						<h2 className="flex flex-row items-center  space-x-2">
-							<p>Software Developer</p>
-							<FiCode />
-							<p>|</p>
-							<p>Self Employ</p>
-							<FiBriefcase />
+					<div className="space-y-2">
+						<div className="flex justify-between items-center">
+							<h1 className="font-bold sm:text-4xl text-2xl cursor-not-allowed uppercase inline-block">
+								{userName}
+							</h1>
+							<span className="flex items-center gap-2">
+								<p className="px-2 dark:bg-zinc-900 bg-zinc-200 rounded-tl-xl rounded-br-xl">
+									aka
+								</p>
+								<h1 className="font-bold sm:text-2xl text-xl cursor-not-allowed uppercase">
+									{nickName}
+								</h1>
+							</span>
+						</div>
+						<h2 className="flex items-center capitalize gap-2 text-xs md:text-base">
+							{bio.map((value, index) => {
+								return (
+									<>
+										<span className="flex items-center gap-1">
+											<p>{value[0]}</p>
+											{value[1]}
+										</span>
+										{index == bio.length - 1 ? "" : "|"}
+									</>
+								);
+							})}
 						</h2>
 					</div>
 					<div>
--- app/components/Stylish/Rainbow.tsx
+++ app/components/Stylish/Rainbow.tsx
@@ -7,7 +7,7 @@ export default function Rainbow({
 }) {
 	return (
 		<div
-		className={`rounded-full brightness-95 absolute z-0 l-0 w-${width} h-${height} blur-3xl rotate-45 animate-pulse hover:animate-none bg-gradient-to-r from-cyan-500 via-lime-800  to-purple-500`}
+		className={`rounded-tl-xl rounded-br-xl opacity-70 absolute z-0 l-0 w-${width} h-${height} blur-3xl rotate-45 animate-pulse hover:animate-none bg-gradient-to-r from-cyan-500 via-lime-800  to-purple-500`}
 		></div>
 	);
 }
