214c8f7128ca4ab60729ad38cb580793e3a4ed93
Houla Sovansela <houlasovansela18@kit.edu.kh>
2023-01-15 20:41:11 +0700

    debugging _app.tst

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 					content="houla sovansela is a software developer"
 				/>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/logo.png" />
+				<link rel="icon" href="/code.png" />
 			</Head>
 			<main className={darkMode}>
 				<div className="flex flex-col h-full w-full absolute bg-white dark:bg-black text-black dark:text-white overflow-hidden">
