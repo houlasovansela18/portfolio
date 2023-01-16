1b17e8990fafcdbdc3ced4d2fae0840a9eea782d
Houla Sovansela <houlasovansela18@kit.edu.kh>
2023-01-16 13:13:37 +0700

    darkMode refactoring to _app

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Comfortaa } from "@next/font/google";
+import { useState } from "react";
+import { DarkModeContext } from "../context/DarkModeContext";
 
 const comfortaa = Comfortaa({
 	subsets: ["latin"],
@@ -8,9 +10,12 @@ const comfortaa = Comfortaa({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+	const [darkMode, setDarkMode] = useState("dark");
 	return (
-		<main className={`${comfortaa.variable} font-sans`}>
-			<Component {...pageProps} />
+		<main className={`${comfortaa.variable} font-sans ${darkMode}`}>
+			<DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
+				<Component {...pageProps} />
+			</DarkModeContext.Provider>
 		</main>
 	);
 }
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,11 +1,8 @@
 import Head from "next/head";
-import { useState } from "react";
 import ResponsiveBodyContainer from "../components/Container/ResponsiveBodyContainer";
 import TopNavbar from "../components/Navbar/TopNavbar";
-import { DarkModeContext } from "../context/DarkModeContext";
 
 export default function Home() {
-	const [darkMode, setDarkMode] = useState("dark");
 	return (
 		<>
 			<Head>
@@ -17,16 +14,12 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/code.png" />
 			</Head>
-			<main className={darkMode}>
-				<div className="flex flex-col h-full w-full absolute bg-white dark:bg-black text-black dark:text-white">
-					<div className="flex-none">
-						<DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
-							<TopNavbar />
-						</DarkModeContext.Provider>
-					</div>
-					<div className="flex-1">
-						<ResponsiveBodyContainer />
-					</div>
+			<main className="flex flex-col h-full w-full absolute bg-white dark:bg-black text-black dark:text-white">
+				<div className="flex-none">
+					<TopNavbar />
+				</div>
+				<div className="flex-1">
+					<ResponsiveBodyContainer />
 				</div>
 			</main>
 		</>
